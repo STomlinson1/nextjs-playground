0fd21a9818e0a0125f7dde1d24f162ca5e1c8fa0
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-09 10:31:07 -0500

    Create a Layout component to wrap the app.

--- ninjalist/pages/index.jsx
+++ ninjalist/pages/index.jsx
@@ -1,16 +1,26 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
-// components
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<div>
-			<Navbar />
 			<h1>Homepage</h1>
-			<Footer />
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maiores
+				illo, eius itaque reiciendis quisquam, saepe officiis architecto qui ipsa
+				repellendus cum consequuntur labore inventore nisi perspiciatis? Beatae,
+				illum totam?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus maiores
+				illo, eius itaque reiciendis quisquam, saepe officiis architecto qui ipsa
+				repellendus cum consequuntur labore inventore nisi perspiciatis? Beatae,
+				illum totam?
+			</p>
+			<Link href="/ninjas">
+				<a>See ninja Listing</a>
+			</Link>
 		</div>
 	);
 }
