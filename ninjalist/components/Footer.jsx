b6c9b0e38f284732bf852a9d6311cfa24aaabc6e
STomlinson1 <53631071+STomlinson1@users.noreply.github.com>
2021-03-09 09:59:31 -0500

    Bringing in external components.

---
+++ ninjalist/components/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+	return <div>Copyright 2021 Ninjalist</div>;
+};
+
+export default Footer;
--- ninjalist/pages/index.jsx
+++ ninjalist/pages/index.jsx
@@ -1,10 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+// components
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 export default function Home() {
 	return (
 		<div>
+			<Navbar />
 			<h1>Homepage</h1>
+			<Footer />
 		</div>
 	);
 }
